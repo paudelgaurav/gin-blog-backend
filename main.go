75b0673395f2d3f0612145f660dc0d6c43009e30
Gaurav <paudelgaurav776@gmail.com>
2022-03-16 15:43:48 +0545

    added blog models

--- main.go
+++ main.go
@@ -4,12 +4,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/paudelgaurav/gin-blog-backend/models"
 )
 
 func main() {
 	router := gin.Default()
+
+	models.ConnectDatabase()
+
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"ping": "pong"})
 	})
+	router.GET("/blogs", getAllBlogs)
 	router.Run()
+}
+
+// get all blogs
+
+func getAllBlogs(c *gin.Context) {
+	var books []models.Blog
+	models.DB.Find(&books)
+	c.JSON(http.StatusOK, gin.H{"data": books})
 }
